8f1e8ae4faf2f48672752f0901d0f704e3c2eaf2
Mahfuzur-Rahman <7mahfuzur.rahman@gmail.com>
2018-01-23 13:14:15 +0600

    Online Shopping Project Initialized

---
+++ OnlineShopping Application/OnlineShopping Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineShopping_Application.Startup))]
+namespace OnlineShopping_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
